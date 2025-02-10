c44df2d2f4525c2fddbc7eb5cf725b100c25fbb3
SeongWook Jo <chosh0206@naver.com>
2025-02-10 20:35:10 +0900

    닉네임 저장으로 변경

--- backend/src/main/java/com/nowtrip/api/config/AuditorAwareImpl.java
+++ backend/src/main/java/com/nowtrip/api/config/AuditorAwareImpl.java
@@ -11,11 +11,23 @@ import java.util.Optional;
 public class AuditorAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
+        // 현재 SecurityContextHolder에서 인증정보 가져오기
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // 인증이 없거나 익명사용자일 경우
         if (authentication == null || !authentication.isAuthenticated()) {
             return Optional.empty();
         }
-        Object principal = authentication.getPrincipal();
-        return Optional.of(authentication.getName());
+
+        // principal이 CustomUserDetails인 경우
+        if (authentication.getPrincipal() instanceof CustomUserDetails customUserDetails) {
+            // 닉네임을 반환하
+            return Optional.of(customUserDetails.getNickname());
+        }
+
+        // 그 외 empty 반환
+        return Optional.empty();
     }
+
+
 }
